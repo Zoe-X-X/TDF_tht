6fc6137416a563477bad0fca5506a7ffd0473abc
Zoe-X-X <ziyueli.zoe@gmail.com>
2021-12-21 10:33:40 +1100

    add permision to lambda for event bridge

--- iaC/eventCreate.py
+++ iaC/eventCreate.py
@@ -1,13 +1,22 @@
 import boto3
 if __name__ == '__main__':
+    # connect to event bridge and lambda clients
     event_client = boto3.client('events')
     lambda_client = boto3.client('lambda')
+    # get lambda arn
     lambda_arn = lambda_client.get_function(FunctionName='getWeatherData')['Configuration']['FunctionArn']
 
-
-    event_client.put_rule(Name='weatherApiSchedule',
+    # set one hour schedule event
+    event_response = event_client.put_rule(Name='weatherApiSchedule',
                                 ScheduleExpression='rate(1 hour)',                                                                            
                                 State='ENABLED')
     event_client.put_targets(Rule='weatherApiSchedule',
                                     Targets=[{'Arn': lambda_arn,
                                                 'Id': '1'}])
+    lambda_client.add_permision(
+        FunctionName=lambda_arn,
+        StatementId='addPermisionForEventBridge',
+        Action='lambda:InvokeFunction',
+        Principal='events.amazonaws.com',
+        SourceArn=event_response['RuleArn']
+    )
